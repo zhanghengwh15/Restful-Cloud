e577bafa80d5e8d365c6e03fc2ac773ad10102c5
zhangheng <123@qq.com>
2022-06-14 07:35:52 +0800

    完成对其他设置

--- src/test/java/com/tool/DataSourceWrapperTest.java
+++ src/test/java/com/tool/DataSourceWrapperTest.java
@@ -4,14 +4,17 @@ import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.RuntimeUtil;
 import cn.hutool.db.ds.DataSourceWrapper;
 import cn.hutool.db.ds.simple.SimpleDataSource;
-import org.junit.Assert;
-import org.junit.Test;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
+
 
 public class DataSourceWrapperTest {
 
 	@Test
+	@SneakyThrows
 	public void cloneTest(){
 
 		String db = FileUtil.getUserHomePath() + "\\restfulCloud\\restfulCloud.db";
@@ -28,7 +31,13 @@ public class DataSourceWrapperTest {
 		final DataSourceWrapper wrapper = new DataSourceWrapper(simpleDataSource, "test.driver");
 
 		// final DataSourceWrapper clone = wrapper.clone();
-		Assert.assertEquals("test.driver", wrapper.getDriver());
-		Assert.assertEquals(simpleDataSource, wrapper.getRaw());
+		assertEquals("test.driver", wrapper.getDriver());
+		assertEquals(simpleDataSource, wrapper.getRaw());
+	}
+
+
+	@Test
+	public void testAdd() {
+		assertEquals(42, Integer.sum(19, 23));
 	}
 }
